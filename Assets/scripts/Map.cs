1b5fb5f2f4bdd093657a9ca7b5d4cc46a2d70443
dieselru <diesel_ru@mail.ru>
2014-06-30 10:45:27 +0900

    first

---
+++ Assets/scripts/Map.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	public Transform bee;
+	public int beeCount;
+
+	private Vector2 mapSize = new Vector2(30, 20);
+	// Use this for initialization
+	void Start () {
+		generateMap();
+	}
+
+	void generateMap()
+	{
+		for (int i = 0; i < beeCount; i++)
+		{
+			Instantiate(bee, new Vector2(Random.Range(0, mapSize.x),
+			                             Random.Range(0, mapSize.y)), bee.rotation);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float transV = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        float transH = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+
+        transform.Translate(new Vector3(transH, transV, 0));
+    }
+}
